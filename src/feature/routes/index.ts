30934f82f820f9be4941381a189c27e53f5c0033
Sang <vuvietsang10a9@gmail.com>
2021-09-10 20:17:06 +0700

    edit Router

--- src/feature/routes/index.ts
+++ src/feature/routes/index.ts
@@ -1,7 +1,7 @@
 import { lazy } from "react";
 import { RouteConfig } from "../../interfaces/routeConfig";
 const Login = lazy(() => import("../login"));
-
+console.log("HIHI");
 const routes: RouteConfig[] = [
   {
     path: "/",
